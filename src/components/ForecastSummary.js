113e5a405288b6a1553b1dc7146202cebca6c91a
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-03-24 19:48:42 +0000

    forecast summary test passing

---
+++ src/components/ForecastSummary.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ForecastSummary(props) {
+  const { date, temperature, description, icon } = props;
+
+  return (
+    <div className="forecast-summary">
+      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        {icon}
+      </div>
+      <div className="forecast-summary__temperature">
+        {temperature.max}
+        &deg;C
+      </div>
+      <div className="forecast-summary__description">{description}</div>
+    </div>
+  );
+}
+
+export default ForecastSummary;
