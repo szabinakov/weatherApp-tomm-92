7f91a22f503a9efdfd8435b78633f4a35676c169
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-03-25 14:49:48 +0000

    all tests passing

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -4,13 +4,13 @@ function ForecastSummary(props) {
   const { date, temperature, description, icon } = props;
 
   return (
-    <div className="forecast-summary">
+    <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
         {icon}
       </div>
       <div className="forecast-summary__temperature">
-        {temperature}
+        {temperature.max}
         &deg;C
       </div>
       <div className="forecast-summary__description">{description}</div>
