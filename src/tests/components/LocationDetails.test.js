120333fe1e1bcb71db0b591f8296b9ba2b61e104
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-04-11 19:18:45 +0100

    location details test updated

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -8,6 +8,6 @@ describe("LocationDetails", () => {
       <LocationDetails city="Manchester" country="UK" />
     );
 
-    expect(getByText("Manchester, UK")).toBeInstanceOf(HTMLHeadingElement);
+    expect(getByText("Manchester UK")).toBeInstanceOf(HTMLHeadingElement);
   });
 });
